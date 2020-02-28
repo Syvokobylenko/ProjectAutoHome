0b417fc526cca71fec53dc5278f52e38eff44a67
Morosov <morosov@pop-os.localdomain>
2020-02-28 18:43:22 +0100

    simplified both output and input socket, now to add gpio

---
+++ inputsocket.py
@@ -0,0 +1,27 @@
+import socket, time
+
+class connection:
+    def __init__(self, IP, port):
+        self.IP = IP
+        self.port = port
+        self.startConnection()
+
+    def startConnection(self):
+        self.s = socket.socket()
+        try:
+            self.s.connect((self.IP, self.port))
+        except(KeyboardInterrupt):
+            exit
+
+        
+    def sendData(self, state):
+        try:
+            self.s.send(str(state))
+        except(KeyboardInterrupt):
+            exit
+    
+
+data = connection("127.0.0.1", 2198)
+
+while True:
+   data.sendData(input("Type 1 or 0 to use switch: "))
---
+++ outputsocket.py
@@ -0,0 +1,19 @@
+import socket
+
+class socketConnection():
+ def __init__(self, port):
+  self.server = socket.socket()
+  self.server.bind(('', port))
+  self.server.listen(5)
+ def acceptCon(self):
+  return self.server.accept()
+
+
+server_instance = socketConnection(2198)
+
+while True:
+ data, addr = server_instance.acceptCon()
+ print ("Got connection from" + str(addr))
+ while True:
+  print(bool(int(data.recv(1).decode())))
+ data.close()
