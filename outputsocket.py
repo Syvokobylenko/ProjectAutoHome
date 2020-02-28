5fefdc4ec16c6ef78b4c0fd445a5eda2724541b6
Morosov <morosov@pop-os.localdomain>
2020-02-28 19:04:00 +0100

    added gpio pins to outputsocket

--- outputsocket.py
+++ outputsocket.py
@@ -1,4 +1,16 @@
-import socket
+import socket, machine
+
+class switchObject():
+ def __init__(self, channel):
+  self.pin = machine.Pin(channel, machine.Pin.OUT)
+
+ def switch(self, state):
+  if state:
+   print("Turning ON")
+   self.pin.on()
+  else:
+   print("Turning OFF")
+   self.pin.off()
 
 class socketConnection():
  def __init__(self, port):
@@ -10,14 +22,20 @@ class socketConnection():
 
 
 server_instance = socketConnection(2198)
+GPIO0Handler = switchObject(0)
 
 while True:
  data, addr = server_instance.acceptCon()
  print ("Got connection from" + str(addr))
+ data.settimeout(5)
  while True:
   try:
-   print(bool(int(data.recv(1).decode())))
+   GPIO0Handler.switch(bool(int(data.recv(1).decode())))
   except(ValueError):
    print("Invalid Input")
+   break
+  except(socket.timeout):
+   print("Timed Out")
+   data.close()
    break
  data.close()
