91702d57209a0b6bd8867902051cd8591985be87
Morosov <syvokobylenko.maksym@protonmail.com>
2020-02-28 10:53:39 +0100

    few changes to gpioclient making new classes

--- gpioclient.py
+++ gpioclient.py
@@ -5,25 +5,30 @@ class switchObject():
  def __init__(self, channel):
   self.pin = machine.Pin(channel, machine.Pin.OUT)
 
- def switch(self, state):
-  if state:
-   self.pin.on()
-  else:
-   self.pin.off()
+def switch(self, state):
+ if state:
+  self.pin.on()
+ else:
+  self.pin.off()
 
-server = socket.socket()
-server.bind(('', 2198))
+class socketConnection():
+ def __init__(self, port):
+  self.server = socket.socket()
+  self.server.bind(('', port))
+  self.server.listen(5)
 
-server.listen(5)
+ def acceptCon(self):
+  return self.server.accept()
 
-switchCreate = switchObject(12)
+switchGPIO0 = switchObject(0)
+server_instance = socketConnection(2198)
 
 while True: 
- data, addr = server.accept()
+ data, addr = server_instance.acceptCon()
  print ('Got connection from' + str(addr))
  while True:
   try:
-   switchCreate.switch(bool(int(data.recv(1).decode())))
+   switchGPIO0.switch(bool(int(data.recv(1).decode())))
   except:
    print(Exception)
  data.close()
